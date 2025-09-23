2bff952d0a3a101f872cd17ffac5a24bfbe7cadc
Darian Esquivel <darianesquivelf@gmail.com>
2025-09-23 17:45:55 -0300

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://darianesquivel.github.io",
-  base: "/bariatrica-integral",
+  base: "/bariatrica-integral/",
   vite: {
     plugins: [tailwindcss()],
   },
