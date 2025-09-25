d63ad9349082e56b43beb9b0fb7b4f31bc47ac2a
Darian Esquivel <darianesquivelf@gmail.com>
2025-09-25 17:03:50 -0300

    deleted extra files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  // site: "https://darianesquivel.github.io",
-  // base: "/bariatrica-integral/",
   vite: {
     plugins: [tailwindcss()],
   },
